e044c0f59f136f9e4e12205fc65e325016bcccef
Amgaa <amgaa@amgaa-CF-S9JYQBDU.(none)>
2013-05-17 12:24:48 +0900

    added

---
+++ server/pwctrl.py
@@ -0,0 +1,18 @@
+# /usr/bin/python
+# Control node power state. It gets the which nodes to turn-on and which ones to turn-off(or hibernate, stand-by ... ). 
+
+class pwctrl:
+    def __init__():
+        self.nachara = "nanchara"
+
+    def turn_on(self, nodes):
+        return 0
+    
+    def turn_off(self, nodes):
+        return 0
+
+    def turn_on_info(self, node):
+        return 0
+
+    def turn_off_info(self, node):
+        return 0
