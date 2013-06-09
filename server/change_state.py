6113e26baaa23f8f9c2bc43fe6d6f742a5e64352
Amgaa <amgaa.hpc@gmail.com>
2013-06-09 22:03:58 +0900

    added

---
+++ server/change_state.py
@@ -0,0 +1,5 @@
+# Changes state of a node. 
+# OFF -> ON
+# ON  -> OFF
+# For now just change the value of nodes in the folder "state/{service, physical}/"
+# If node does not exists, just create it. 
---
+++ server/try_on_off.py
@@ -0,0 +1,3 @@
+# Tries to change states of nodes according to the information from get_on_off.py by checking their dependencies and their childs' states. 
+# Tries to make all necessary nodes ON 
+# Tries to make all unnecessary nodes OFF
