ecd13bab271b18b0127c4011ec8bcfbcd0695542
Amgaa <amgaa.hpc@gmail.com>
2014-01-13 19:58:01 +0900

    From commiter

--- bin/cntools.py
+++ bin/cntools.py
@@ -385,8 +385,10 @@ def action_pbsnodes(pbs_release):
     qnodes = qsub_nodes()
     tnodes = torque_nodes()
     check_and_request(qnodes)
-#    log.info("ACTION_PBSNODES called")
-#    log.info("PBS_RELEASE VALUE is: "  + str(PBS_RELEASE))
+    log.info("ACTION_PBSNODES called")
+    log.info("PBS_RELEASE VALUE is: "  + str(PBS_RELEASE))
+    log.info("qsub nodes: " + str(qnodes))
+    log.info("torque nodes: " + str(tnodes))
     if pbs_release:
         rel_nodes = [node for node in tnodes if not node in qnodes]
         check_and_release(rel_nodes)
